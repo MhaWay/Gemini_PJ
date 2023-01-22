c83e335275e938000f489fdc529f9964d2395aec
Manu <manu573@live.it>
2023-01-22 13:36:28 +0100

    new update

--- src/sensors/SensorManager.cpp
+++ src/sensors/SensorManager.cpp
@@ -20,27 +20,28 @@ namespace SlimeVR {
             std::vector<uint8_t> addresses = scanI2CAddresses();
             m_multiplexer->begin();
             uint8_t address;
+        
             for (int channel = 0; channel < 8; ++channel) {
                 m_multiplexer->selectChannel(channel);
-                if (channel < addresses.size()) {
-                    address = addresses[channel];
-                    switch(address) {
-                        case BNO080_I2C_ADDRESS:
-                            m_Sensors[channel] = new BNO080Sensor(channel, SENSOR_TYPE_IMU, address, ROTATION_270, PIN_IMU_INT);
-                            break;
-                        case LSM9DS1_I2C_ADDRESS:
-                            m_Sensors[channel] = new LSM9DS1Sensor(channel, address, ROTATION_270);
-                            break;
-                        default:
-                            m_Sensors[channel] = new ErroneousSensor(channel, address);
-                            break;
+                for (int i = 0; i < addresses.size(); ++i) {
+                    if (addresses[i] != 0) {
+                        //initializing specific sensor here
+                        switch(addresses[i]) {
+                            case BNO080_I2C_ADDRESS:
+                                m_Sensors[channel] = new BNO080Sensor(channel, SENSOR_TYPE_IMU, addresses[i], ROTATION_270, PIN_IMU_INT);
+                                break;
+                            default:
+                                break;
+                        }
+                        m_Sensors[channel]->setup();
+                    } else {
+                        //initializing empty sensor
+                        m_Sensors[channel] = new EmptySensor(channel);
                     }
-                    m_Sensors[channel]->setup();
-                } else {
-                    m_Sensors[channel] = new EmptySensor(channel);
                 }
             }
         }
+
 
 
         void SensorManager::postSetup() {
